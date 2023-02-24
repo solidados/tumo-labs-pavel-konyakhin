8ad9314b61b8383efa95bb1628d1702248ef1a77
solidados <geekbrains.pk@internet.ru>
2023-02-24 20:06:56 +0400

    feat: appCount b/fore sentences

--- word-counter-pavel-konyakhin/src/js/main.js
+++ word-counter-pavel-konyakhin/src/js/main.js
@@ -1,13 +1,42 @@
-document.addEventListener("DOMContentLoaded", () => {
-	"use strict";
+const entryField = document.querySelector(".main__input");
+const calcButton = document.querySelector(".calculate__btn");
 
-	const entryField = document.querySelector(".main__input");
-	const resultField = document.querySelector(".main__table-list");
-	const calcButton = document.querySelector(".calculate__btn");
-	//   const targetElement = event.target;
+function wordsCount(str) {
+	let arr = str.split(" ");
+	document.getElementById("words").innerText = arr.length;
+}
 
-	const getTotalLength = () => console.log(entryField.value.length);
+function lettersCount(str) {
+	let letters = str.split(" ").join("");
+	document.getElementById("letters").innerText = letters.length;
+}
 
-	calcButton.addEventListener("click", getTotalLength);
-});
-// new comment
+function spacesCount(str) {
+	document.getElementById("spaces").innerText = str
+		.split("")
+		.filter((el) => el === " ").length;
+}
+
+function numbersCount(str) {
+	document.getElementById("numbers").innerText = str.match(/(\d)/g).length;
+}
+
+function symbolsCount(str) {
+	document.getElementById("symbols").innerText = str
+		.split(" ")
+		.join("")
+		.match(/\W/g).length;
+}
+
+// ! --- final count ---
+function appCount() {
+	let entry = entryField.value;
+
+	wordsCount(entry);
+	lettersCount(entry);
+	spacesCount(entry);
+	numbersCount(entry);
+	symbolsCount(entry);
+}
+
+calcButton.addEventListener("click", appCount);
